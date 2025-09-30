116fff220b3696c749f06f53c8d04b9a46945e07
Jannescynatix <jannesarda@gmx.de>
2025-09-30 16:58:35 +0200

    Gzugouzgodd

--- ado-dashboard-proff/src/main.ts
+++ ado-dashboard-proff/src/main.ts
@@ -3,12 +3,14 @@ import { createPinia } from 'pinia';
 import router from './router';
 import App from './App.vue';
 import './style.css';
-import VueGtag from 'vue-gtag'
+import { createGtag } from 'vue-gtag'
 
 const app = createApp(App);
 
-app.use(VueGtag, {
-    config: { id: 'G-KR6Q5TQTTN' } // <-- deine GA4 Measurement ID hier einfügen
+app.use(createGtag, {
+    property: {
+        id: 'G-KR6Q5TQTTN' // <-- deine GA4 Measurement ID
+    }
 }, router)
 
 app.use(createPinia());
