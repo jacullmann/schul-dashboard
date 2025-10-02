451fdf9d6441e5081a56e58cd1f3ee8d845faa09
Jannescynatix <jannesarda@gmx.de>
2025-10-02 17:16:54 +0200

    GougzuoguzgO

--- ado-dashboard-proff/src/main.ts
+++ ado-dashboard-proff/src/main.ts
@@ -11,7 +11,7 @@ const app = createApp(App);
 function initAnalytics() {
     if (!(app as any)._context.provides['vue-gtag']) {
         app.use(VueGtag, {
-            property: { id: 'G-KR6Q5TQTTN' }
+            property: { id: 'G-Y9XSGT1NL4' }
         }, router)
     }
 }
