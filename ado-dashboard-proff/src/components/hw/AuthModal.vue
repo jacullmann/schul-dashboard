cb667c1bc9314d5de8ebfde4ca1c1b94dc4cb3fa
Jannescynatix <jannesarda@gmx.de>
2025-10-11 14:36:07 +0200

    Gzuoguzo

--- ado-dashboard-proff/src/components/hw/AuthModal.vue
+++ ado-dashboard-proff/src/components/hw/AuthModal.vue
@@ -54,10 +54,7 @@
       <!-- Submit -->
       <div class="row" style="margin-top:12px; align-items:center;">
         <button class="btn" @click="submit">
-          <svg v-if="submitting" class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-          </svg>
+          <LoadingSpinner v-if="submitting" color="black" size="1.2em" />
           {{ mode==='login' ? 'Anmelden' : 'Registrieren' }}
         </button>
 
@@ -70,6 +67,7 @@
 <script setup lang="ts">
 import { ref, reactive } from 'vue';
 import hw from '../../hwApi';
+import LoadingSpinner from "../LoadingSpinner.vue";
 
 const emit = defineEmits<{ (e: 'close'): void; (e: 'logged-in', token: string): void }>();
 
