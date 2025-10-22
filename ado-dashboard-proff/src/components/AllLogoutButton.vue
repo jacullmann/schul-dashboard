56238dcea217a632da89a6faf5b78fc37b33383a
Jannescynatix <jannesarda@gmx.de>
2025-10-23 00:11:40 +0200

    GZugozuoizrfi3erw3r2eeeewwwwweeeeeeeeeeeeeeeeerrrzzz3we3e

--- ado-dashboard-proff/src/components/AllLogoutButton.vue
+++ ado-dashboard-proff/src/components/AllLogoutButton.vue
@@ -27,7 +27,7 @@ function openPopup() {
 }
 function doLogout() {
   auth.logout();
-  window.location.href ='https://schul-dashboards.onrender.com'
+  window.location.replace('https://schul-dashboards.onrender.com')
 }
 
 function closePopup() {
