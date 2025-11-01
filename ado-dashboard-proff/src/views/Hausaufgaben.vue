7c99f6247e1330432a4050e1cc688913c1d7620b
Jannescynatix <jannesarda@gmx.de>
2025-11-01 19:40:53 +0100

    zugizugzugoefefefefefef

--- ado-dashboard-proff/src/views/Hausaufgaben.vue
+++ ado-dashboard-proff/src/views/Hausaufgaben.vue
@@ -124,7 +124,7 @@
             <button data-umami-event="Dashboard Bilder verwalten Button" class="menu-btn" v-if="user" @click="onMenuAction('images', item)">
               <div class="fixall">
                 <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#e3e3e3"><path d="M0 0h24v24H0z" fill="none"/><path d="M21 19V5c0-1.1-.9-2-2-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2zM8.5 13.5l2.5 3.01L14.5 12l4.5 6H5l3.5-4.5z"/></svg>
-                Bilder verwalten
+                Bilder
               </div>
             </button>
             <button data-umami-event="Dashboard bearbeiten Button" class="menu-btn" v-if="canManage(item.createdBy)" @click="onMenuAction('edit', item)">
