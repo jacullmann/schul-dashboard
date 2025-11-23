1119af32f6037e2f46be94e22d5e8ec3b14b1479
Jannescynatix <jannesarda@gmx.de>
2025-11-23 18:17:36 +0100

    hetzen test

--- backup2hwserver/server.js
+++ backup2hwserver/server.js
@@ -86,4 +86,4 @@ routes(app, {
 
 app.get('/health', (req, res) => res.json({ ok: true }));
 
-app.listen(PORT, () => console.log(`Hausaufgaben backend auf dem :${PORT}`));
+app.listen(PORT, () => console.log(`Hausaufgaben BBBackend auf dem :${PORT}`));
