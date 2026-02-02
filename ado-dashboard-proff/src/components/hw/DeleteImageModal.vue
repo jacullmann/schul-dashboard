7f3e83cce680591bdea56a2b7d2173bd9d495aaf
Jannescynatix <jannesarda@gmx.de>
2026-02-02 10:07:09 +0100

    max lebenswerk form deleteimage

--- ado-dashboard-proff/src/components/hw/DeleteImageModal.vue
+++ ado-dashboard-proff/src/components/hw/DeleteImageModal.vue
@@ -1,30 +1,27 @@
 <template>
-  <div v-if="show" class="blurit" @click.stop="$emit('cancel')">
-    <div class="delete-box" @click.stop>
-      <h4 style="margin-top: 0">Dieses Bild löschen?</h4>
-      <p>Wenn du dieses Bild löschst, wird es unwiderruflich entfernt.</p>
+  <Modal v-if="show" @cancel="$emit('cancel')">
+    <template #title>
+      Dieses Bild löschen?
+    </template>
 
-      <div class="row">
-        <button
-            class="btn ghost"
-            @click="$emit('cancel')"
-            data-umami-event="Bild löschen abbrechen"
-        >
-          Abbrechen
-        </button>
-        <button
-            class="btn danger"
-            @click="$emit('confirm')"
-            data-umami-event="Bild endgültig löschen bestätigen"
-        >
-          Bild löschen
-        </button>
-      </div>
-    </div>
-  </div>
+    <template #content>
+      <p>Wenn du dieses Bild löschst, wird es unwiderruflich entfernt.</p>
+    </template>
+
+    <template #action-btn>
+      <button
+          class="btn danger"
+          @click="$emit('confirm')"
+      >
+        Bild löschen
+      </button>
+    </template>
+  </Modal>
 </template>
 
 <script setup lang="ts">
+import Modal from './Modal.vue';
+
 defineProps<{
   show: boolean
 }>()
@@ -33,26 +30,10 @@ defineEmits(['confirm', 'cancel'])
 </script>
 
 <style scoped>
-.delete-box {
-  background: var(--lbg);
-  padding: 16px;
-  border-radius: 16px;
-  max-width: 420px;
-  width: 90%;
+p {
   text-align: left;
-  border: 1px solid var(--border);
-  box-shadow: var(--shadow-l);
-}
-
-.delete-box h4 {
-  color: var(--text);
-  margin-bottom: 12px;
-}
-
-.delete-box p {
-  text-align: left;
-  margin-bottom: 16px;
-  font-size: 14px;
+  margin-block: 0 16px;
+  font-size: var(--font-size-body);
   color: var(--text);
   line-height: 1.5;
 }
