4e15d692ca86f9c6d38b910b52bf8260edd72392
schuldashboard <dashboardverifizierung@gmail.com>
2026-06-07 20:46:57 +0200

    improve basetoggle design

--- app/src/common/components/BaseToggle.vue
+++ app/src/common/components/BaseToggle.vue
@@ -22,16 +22,24 @@ const toggle = () => {
 
 const buttonClasses = computed(() => {
   return [
-    'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-zinc-900 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+    'group relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out disabled:cursor-not-allowed disabled:opacity-50',
     model.value ? 'bg-accent' : 'bg-ghost',
   ];
 });
 
-const dotClasses = computed(() => {
+const dotWrapperClasses = computed(() => {
   return [
-    'pointer-events-none inline-block h-5 w-5 bg-white transform rounded-full shadow-sm ring-0 transition duration-200 ease-in-out',
+    'pointer-events-none relative inline-block size-5 transform transition-transform duration-200 ease-in-out',
     model.value ? 'translate-x-5' : 'translate-x-0',
   ];
+});
+
+const hoverClasses = computed(() => {
+  return 'absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-8 rounded-full bg-[oklch(1_0_0/40%)] opacity-0 scale-70 group-hover:opacity-100 group-hover:scale-100 group-disabled:opacity-0 transition duration-150 ease-in-out';
+});
+
+const dotClasses = computed(() => {
+  return 'absolute inset-0 bg-white transform rounded-full ring-0 transition-transform scale-100 group-active:scale-80 duration-200 ease-in-out';
 });
 </script>
 
@@ -47,7 +55,10 @@ const dotClasses = computed(() => {
     >
       <span v-if="label" class="sr-only">{{ label }}</span>
 
-      <span aria-hidden="true" :class="dotClasses"></span>
+      <span aria-hidden="true" :class="dotWrapperClasses">
+        <span :class="hoverClasses"></span>
+        <span :class="dotClasses"></span>
+      </span>
     </button>
 
     <span
