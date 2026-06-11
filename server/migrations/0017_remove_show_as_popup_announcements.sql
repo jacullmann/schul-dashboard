29884fc817606fe21f4c27582df81ab5195b0a49
Jannescynatix <jannesarda@gmx.de>
2026-06-11 17:14:25 +0200

    add two sqlx migrations to fix legacy schemes

---
+++ server/migrations/0017_remove_show_as_popup_announcements.sql
@@ -0,0 +1,2 @@
+ALTER TABLE public.announcements
+DROP COLUMN show_as_popup;
---
+++ server/migrations/0018_remove_get_unread_status_and_upsert_user_tenant_visit_functions.sql
@@ -0,0 +1,2 @@
+DROP FUNCTION public.get_unread_status(uuid);
+DROP FUNCTION public.upsert_user_tenant_visit(uuid, uuid, text);
