34c6a029230466a307f129eac0ef20af002c4141
schuldashboard <dashboardverifizierung@gmail.com>
2026-09-18 00:34:28 +0200

    add bottom margin to basesheet

--- app/src/common/components/BaseSheet.vue
+++ app/src/common/components/BaseSheet.vue
@@ -3,7 +3,7 @@ import { ref, watch } from 'vue';
 
 defineOptions({ inheritAttrs: false });
 
-withDefaults(
+const props = withDefaults(
   defineProps<{
     open?: boolean;
   }>(),
@@ -230,7 +230,7 @@ defineExpose({ sheetEl });
         v-if="open"
         ref="sheetEl"
         v-bind="$attrs"
-        class="fixed bottom-0 left-0 right-0 z-[var(--z-tooltip)] bg-surface border-t border-ghost-border rounded-t-2xl shadow-menu max-h-[85dvh] overflow-y-auto overscroll-contain"
+        class="fixed bottom-0 left-0 right-0 z-[var(--z-tooltip)] pb-4 bg-surface border-t border-ghost-border rounded-t-2xl shadow-menu max-h-[85dvh] overflow-y-auto overscroll-contain"
         @click.stop
         @touchstart="onTouchStart"
         @touchmove="onTouchMove"
