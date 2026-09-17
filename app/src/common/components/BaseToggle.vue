e337d62d5cdc7c74df872a5876657a39922405f6
schuldashboard <dashboardverifizierung@gmail.com>
2026-09-17 08:48:46 +0200

    improve basetoggle design and fade scale transition

--- app/src/common/components/BaseToggle.vue
+++ app/src/common/components/BaseToggle.vue
@@ -22,20 +22,20 @@ const toggle = () => {
 
 const buttonClasses = computed(() => {
   return [
-    'group relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out disabled:cursor-not-allowed disabled:opacity-50 touch-target after:min-h-12 after:min-w-12',
+    'group relative inline-flex h-6 w-14 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-150 ease-in-out disabled:cursor-not-allowed disabled:opacity-50 touch-target after:min-h-12 after:min-w-12',
     model.value ? 'bg-accent' : 'bg-ghost',
   ];
 });
 
 const dotWrapperClasses = computed(() => {
   return [
-    'pointer-events-none relative inline-block size-5 transform transition-transform duration-200 ease-in-out',
+    'pointer-events-none relative inline-block h-5 w-8 transform transition-transform duration-150 ease-in-out',
     model.value ? 'translate-x-5' : 'translate-x-0',
   ];
 });
 
 const hoverClasses = computed(() => {
-  return 'absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-8 rounded-full bg-[oklch(0.8_0_0/40%)] opacity-0 scale-70 group-hover:opacity-100 group-hover:scale-100 group-disabled:opacity-0 transition duration-150 ease-in-out';
+  return 'absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-8 w-11 rounded-full bg-[oklch(0.8_0_0/40%)] opacity-0 scale-70 group-hover:opacity-100 group-hover:scale-100 group-disabled:opacity-0 transition duration-150 ease-in-out';
 });
 
 const dotClasses = computed(() => {
